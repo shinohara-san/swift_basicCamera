f843f04286ee52623c5b2b46a6c5517d7a39a98b
shinohara-san <someiyoshino0809@gmail.com>
2020-08-20 15:01:04 +0900

    Initial Commit

---
+++ TakingPictures/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TakingPictures
+//
+//  Created by Yuki Shinohara on 2020/08/20.
+//  Copyright © 2020 Yuki Shinohara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
